a69b0b5f134c2601908e5ecf3e1dc0bc269fb2cf
Shivam Trivedi <shivtrivedi197@gmail.com>
2023-09-07 22:25:51 +0530

    final commit

---
+++ config.js
@@ -0,0 +1,21 @@
+var config = {
+  // The following sentence does not have to be 7 sentences, you can also change it to 4 sentences, 2 sentences are fine
+  lines: [
+    "Dear bestiiii",
+    "Flowers bloom for a season, as evidenced by falling reds",
+    "How can the passing years be entrusted to the smoke and dust",
+    "Your smile is my eternal attachment",
+    "Can you understand the true feelings of lovesickness?",
+    "Dear bachu, you are the only one for me",
+    "love you forever...",
+  ],
+  // The time of falling in love Remember that the format cannot be written wrong, it is very important
+  // The format must be the same as the time format below
+  // are year-month-day T hour:minute:second
+  // Warm warning:
+  // 1. The hour, minute and second must be two numbers, such as 00:00:00
+  // 2.T cannot be lost
+  date: "2023-02-17T18:30:30",
+  // Names of both parties
+  names: ["Dayu bachu", "Gnado puchko"],
+};
